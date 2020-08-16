f1d9d8f62acad33db2a1cfde5d0a210969550401
ZenoHuang07 <zenohuang07@gmail.com>
2020-08-16 15:36:22 +0800

    add bottom sheet behavior

--- app/src/main/java/com/cartrack/assignment/ui/main/MainActivity.kt
+++ app/src/main/java/com/cartrack/assignment/ui/main/MainActivity.kt
@@ -4,13 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import com.cartrack.assignment.R
 import com.cartrack.assignment.ui.base.BaseActivity
-import com.cartrack.assignment.ui.login.LoginActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.sub_bottom_sheet.*
 
-class MainActivity : BaseActivity() , OnMapReadyCallback {
+class MainActivity : BaseActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
+    private val bottomSheet by lazy {
+        BottomSheetBehavior.from(clBottomSheet)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +26,34 @@ class MainActivity : BaseActivity() , OnMapReadyCallback {
         mMap = googleMap
     }
 
-    private fun initViews(){
+    private fun initViews() {
 
         initMap()
+        setTouchEffect()
+        setClickListener()
     }
 
-    private fun initMap(){
+    private fun initMap() {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this@MainActivity)
+    }
+
+    private fun setTouchEffect() {
+        viewOpenBar.setOnTouchListener { _, motionEvent ->
+            openBar.onTouchEvent(motionEvent)
+            false
+        }
+    }
+
+    private fun setClickListener() {
+        viewOpenBar.setOnClickListener {
+            if (bottomSheet.state == BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheet.state = BottomSheetBehavior.STATE_COLLAPSED
+            } else if (bottomSheet.state == BottomSheetBehavior.STATE_COLLAPSED) {
+                bottomSheet.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        }
     }
 
     companion object {
